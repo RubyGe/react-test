cf4f13630507a2f68865c6631be0e55ca67525ae
Genie Guo <genie@intventures.co>
2019-02-27 20:57:04 -0800

    updated app.js fil

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Text, TextInput, View, Button } from 'react-native';
+import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
 
-export default class  extends Component {
+export default class Touchables extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = { count: 0 }
+  }
+
+  onPress = () => {
+    this.setState({ count: this.state.count + 1})
   }
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-          autoCorrect={false}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}</Text>
-      <Button
-        onPress={() => {
-          Alert.alert('You tapped the button!');
-        }}
-        title="Press Me"
-      />
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.button} onPress={this.onPress}>
+          <Text style={styles.buttonText}>Touch Here</Text>
+        </TouchableHighlight>
+        <View>
+          <Text>{this.state.count}</Text>
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  }
+});
+
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+AppRegistry.registerComponent('AwesomeProject', () => Touchables);
